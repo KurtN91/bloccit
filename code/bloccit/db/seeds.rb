2ee99cf735516269949ef23d358bd2aab7c91a82
Kurt Neaderhiser <kurt.neaderhiser@gmail.com>
2017-04-28 17:13:09 -0400

    Checkpoint 17 done

--- code/bloccit/db/seeds.rb
+++ code/bloccit/db/seeds.rb
@@ -1,7 +1,34 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+ require 'random_data'
+
+ # Create Posts
+ 50.times do
+ # #1
+   Post.create!(
+ # #2
+     title:  RandomData.random_sentence,
+     body:   RandomData.random_paragraph
+   )
+ end
+ posts = Post.all
+ 
+ # Create Comments
+ # #3
+ 100.times do
+   Comment.create!(
+ # #4
+     post: posts.sample,
+     body: RandomData.random_paragraph
+   )
+ end
+ 
+ puts "#{Post.count}"
+Post.find_or_create_by!(title: "This is unique", body: "this is also unique")
+ puts "#{Post.count}"
+ 
+ puts "#{Comment.count}"
+ Comment.find_or_create_by(post_id: "151" , body: "This is unique too")
+ puts "#{Comment.count}"
+ 
+ puts "Seed finished"
+ puts "#{Post.count} posts created"
+ puts "#{Comment.count} comments created"
